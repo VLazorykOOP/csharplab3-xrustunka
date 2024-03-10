7a1d0d548a5f693df63607dfdea96178b2dbf881
Христина <lavruk.khrystyna.clg@chnu.edu.ua>
2024-03-10 21:07:44 +0200

    Завдання 1-2

--- Lab3CSharp/Program.cs
+++ Lab3CSharp/Program.cs
@@ -1,10 +1,121 @@
-﻿namespace Lab3CSharp
+﻿using System;
+
+class Point
 {
-    internal class Program
+    private int x;
+    private int y;
+    private ConsoleColor color;
+
+    // Конструктор з нульовими координатами
+    public Point()
     {
-        static void Main(string[] args)
+        x = 0;
+        y = 0;
+        color = ConsoleColor.White; // Білий колір за замовчуванням
+    }
+
+    // Конструктор з заданими координатами
+    public Point(int x, int y, ConsoleColor color)
+    {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    // Метод для виведення координат точки на екран
+    public void PrintCoordinates()
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine($"Координати точки: ({x}, {y})");
+        Console.ResetColor();
+    }
+
+    // Метод для розрахунку відстані від початку координат до точки
+    public double DistanceToOrigin()
+    {
+        return Math.Sqrt(x * x + y * y);
+    }
+
+    // Метод для переміщення точки на вектор з заданими координатами
+    public void Move(int x1, int y1)
+    {
+        x += x1;
+        y += y1;
+    }
+
+    // Властивість для отримання і встановлення координат точки
+    public int X
+    {
+        get { return x; }
+        set { x = value; }
+    }
+
+    public int Y
+    {
+        get { return y; }
+        set { y = value; }
+    }
+
+    // Властивість тільки для читання для отримання коліру точки
+    public ConsoleColor Color
+    {
+        get { return color; }
+    }
+}
+
+class Program
+{
+    static void Main1(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        // Введення координат точки
+        Console.WriteLine("Введіть координати точки (x, y):");
+        int x = Convert.ToInt32(Console.ReadLine());
+        int y = Convert.ToInt32(Console.ReadLine());
+
+        // Введення коліру точки
+        Console.WriteLine("Введіть колір для точки (0 - Чорний, 1 - Білий, 2 - Червоний, 3 - Зелений, 4 - Синій):");
+        int colorInput = Convert.ToInt32(Console.ReadLine());
+
+        ConsoleColor color = ConsoleColor.White; // Білий колір за замовчуванням
+
+        switch (colorInput)
         {
-            Console.WriteLine("Lab 3 ");
+            case 0:
+                color = ConsoleColor.Black;
+                break;
+            case 1:
+                color = ConsoleColor.White;
+                break;
+            case 2:
+                color = ConsoleColor.Red;
+                break;
+            case 3:
+                color = ConsoleColor.Green;
+                break;
+            case 4:
+                color = ConsoleColor.Blue;
+                break;
+            default:
+                Console.WriteLine("Неправильний ввід. Встановлено білий колір за замовчуванням.");
+                break;
         }
+
+        // Створення екземпляру класу Point
+        Point point = new Point(x, y, color);
+
+        // Виведення інформації про точку та відстань до центра координат
+        point.PrintCoordinates();
+        Console.WriteLine($"Відстань до центра координат: {point.DistanceToOrigin()}");
+
+        // Переміщення точки на заданий вектор
+        Console.WriteLine("Введіть вектор для переміщення (x1, y1):");
+        int x1 = Convert.ToInt32(Console.ReadLine());
+        int y1 = Convert.ToInt32(Console.ReadLine());
+        point.Move(x1, y1);
+
+        // Виведення оновлених координат точки
+        Console.WriteLine("Після переміщення:");
+        point.PrintCoordinates();
     }
 }
---
+++ Lab3CSharp/Program2.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    public virtual void Show()
+    {
+        Console.WriteLine($"Name: {Name}, Age: {Age}");
+    }
+}
+
+class Student : Person
+{
+    public int StudentId { get; set; }
+    public string Major { get; set; }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Student ID: {StudentId}, Major: {Major}");
+    }
+}
+
+class Teacher : Person
+{
+    public string Department { get; set; }
+    public string Subject { get; set; }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Department: {Department}, Subject: {Subject}");
+    }
+}
+
+class Staff : Person
+{
+    public string Position { get; set; }
+    public int Salary { get; set; }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Position: {Position}, Salary: {Salary}");
+    }
+}
+
+class DepartmentChair : Teacher
+{
+    public string DepartmentName { get; set; }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($"Department Name: {DepartmentName}");
+    }
+}
+
+class Program2
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        List<Person> people = new List<Person>();
+
+        Console.WriteLine("Введіть інформацію про студента:");
+        Student student = new Student();
+        Console.Write("Ім'я: ");
+        student.Name = Console.ReadLine();
+        Console.Write("Вік: ");
+        student.Age = Convert.ToInt32(Console.ReadLine());
+        Console.Write(" ID: ");
+        student.StudentId = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Статус: ");
+        student.Major = Console.ReadLine();
+        people.Add(student);
+
+        Console.WriteLine("\nВведіть інформацію про вчителя:");
+        Teacher teacher = new Teacher();
+        Console.Write("Ім'я: ");
+        teacher.Name = Console.ReadLine();
+        Console.Write("Вік: ");
+        teacher.Age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Кафедра: ");
+        teacher.Department = Console.ReadLine();
+        Console.Write("Предмет: ");
+        teacher.Subject = Console.ReadLine();
+        people.Add(teacher);
+
+        Console.WriteLine("\nВведіть інформацію про персонал:");
+        Staff staff = new Staff();
+        Console.Write("Ім'я: ");
+        staff.Name = Console.ReadLine();
+        Console.Write("Вік: ");
+        staff.Age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Статус: ");
+        staff.Position = Console.ReadLine();
+        Console.Write("Зарплата: ");
+        staff.Salary = Convert.ToInt32(Console.ReadLine());
+        people.Add(staff);
+
+        Console.WriteLine("\nВведіть інформацію про завідувача кафедри:");
+        DepartmentChair chair = new DepartmentChair();
+        Console.Write("Ім'я: ");
+        chair.Name = Console.ReadLine();
+        Console.Write("Вік: ");
+        chair.Age = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Кафедра: ");
+        chair.Department = Console.ReadLine();
+        Console.Write("Предмет: ");
+        chair.Subject = Console.ReadLine();
+        Console.Write(" Спеціальність: ");
+        chair.DepartmentName = Console.ReadLine();
+        people.Add(chair);
+
+        Console.WriteLine("\nВідсортований список за назвою:");
+        people.Sort((p1, p2) => p1.Name.CompareTo(p2.Name));
+
+        // Виведення інформації про всіх об'єкти масиву
+        foreach (var person in people)
+        {
+            person.Show();
+            Console.WriteLine();
+        }
+    }
+}
